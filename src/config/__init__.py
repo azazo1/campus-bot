ec9976bb6a6ebc1f19b7b9490b14bb48a8536ce4
Deralive <10235101526@stu.ecnu.edu.cn>
2024-11-13 18:36:28 +0800

    Add Doc

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,10 +1,11 @@
 import logging
 import sys
+from typing import Optional
 
 import toml
 
 LOG_FILE = "ec-plugin.log"
-CONFIG_FILE = "info.toml"
+CONFIG_FILE = "configuration.toml"
 
 SMTP_HOST = ""
 SMTP_PASS = ""
@@ -12,27 +13,36 @@ SMTP_USER = ""
 SMTP_FROM = None
 SMTP_TO = None
 
-logger = None
+logger: Optional[logging.Logger] = None
 
 
-def _read_config_file():
+def _read_config_file() -> dict:
     with open(CONFIG_FILE, 'r', encoding="utf-8") as f:
         return toml.load(f)
 
 
 def _load_email(config: dict):
-    global SMTP_HOST
-    global SMTP_PASS
-    global SMTP_USER
-    global SMTP_FROM
-    global SMTP_TO
-    # 访问配置数据
+    '''
+    # Example
+
+    ```toml
+    # configuration.toml
+    [smtp]
+    host = "smtp.qq.com" # smtp server
+    pass = "password" # SMTP 服务器的 Token
+    user = "sender_email @ qq.com" # 用来发邮件的邮箱
+    from = ["name", "email"] # 发件人的名字和邮箱
+    to = ["name", "email"] # 收件人的名字和邮箱
+    ```
+
+    '''
+    global SMTP_HOST, SMTP_PASS, SMTP_USER, SMTP_FROM, SMTP_TO
     SMTP_HOST = config["smtp"]["host"]
     SMTP_PASS = config["smtp"]["pass"]
     SMTP_USER = config["smtp"]["user"]
     SMTP_FROM = (config["smtp"]["from"][0], config["smtp"]["from"][1])
     SMTP_TO = (config["smtp"]["to"][0], config["smtp"]["to"][1])
-    logging.info((SMTP_HOST, SMTP_PASS, SMTP_USER, SMTP_FROM, SMTP_TO))
+    logger.info((SMTP_HOST, SMTP_PASS, SMTP_USER, SMTP_FROM, SMTP_TO))
 
 
 def _init_logger():
@@ -57,7 +67,3 @@ def init():
     _init_logger()
     config = _read_config_file()
     _load_email(config)
-
-
-if __name__ == "__main__":
-    init()
