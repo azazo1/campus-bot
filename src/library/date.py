0fe7d7b8098db0c721483b06a6bdc225148991ee
azazo1 <azazo1@qq.com>
2024-11-14 22:04:41 +0800

    date modifies
    - Time 类改名为 TimePeriod, 更加直观.
    - TimePeriod 类现在会持有 Day 的引用, 以实现在 query_seats 方法中传入 TimePeriod 即可确认要查询的时间段.
    - TimePeriod 的 __getattr__ 方法修改为 __getitem__ 防止混淆.
    - Day 提供 __getitem__ 方法用于从 json 中提取内容.

--- src/library/date.py
+++ src/library/date.py
@@ -1,36 +1,9 @@
 """
-图书馆特定区域的可预约时间对象.
+图书馆特定区域的可预约时间对象, 见 $projectdir/test/test_library/query_date_example.json 文件.
 """
 from typing import Self
 
-from datetime import time, date, datetime
-
-
-class Time:
-    """一天中的可选时间段."""
-
-    def __init__(self, json_data: dict):
-        """
-        Parameters:
-            json_data: 一个 times 中的对象.
-        """
-        self.raw = json_data
-        self.id = int(self.raw['id'])
-        start = self.raw['start']
-        end = self.raw['end']
-        self.start = time(*[int(i) for i in start.split(':', maxsplit=2)])
-        self.end = time(*[int(i) for i in end.split(':', maxsplit=2)])
-
-    @classmethod
-    def from_response_part(cls, part: list):
-        """从 date 请求返回 json 中的 times 字段提取出多个 Time. """
-        return [Time(p) for p in part]
-
-    def __getattr__(self, item):
-        return self.raw[item]
-
-    def __repr__(self):
-        return repr(self.raw)
+from datetime import time, datetime
 
 
 class Day:
@@ -46,7 +19,7 @@ class Day:
         self.raw = json_data
         day_ = self.raw["day"]
         self.day = datetime.strptime(day_, "%Y-%m-%d").date()
-        self.times = Time.from_response_part(self.raw["times"])
+        self.times = TimePeriod.from_response_part(self.raw["times"], self)
 
     @classmethod
     def from_response(cls, json_data: list[dict]) -> list[Self]:
@@ -57,6 +30,37 @@ class Day:
         for obj in json_data:
             rst.append(Day(obj))
         return rst
+
+    def __repr__(self):
+        return repr(self.raw)
+
+    def __getitem__(self, item):
+        return self.raw[item]
+
+
+class TimePeriod:
+    """一天中的可选时间段."""
+
+    def __init__(self, json_data: dict, day: Day):
+        """
+        Parameters:
+            json_data: 一个 times 中的对象.
+        """
+        self.raw = json_data
+        self.id = int(self.raw['id'])
+        start = self.raw['start']
+        end = self.raw['end']
+        self.day = day
+        self.start = time(*[int(i) for i in start.split(':', maxsplit=2)])
+        self.end = time(*[int(i) for i in end.split(':', maxsplit=2)])
+
+    @classmethod
+    def from_response_part(cls, part: list, day):
+        """从 date 请求返回 json 中的 times 字段提取出多个 Time. """
+        return [TimePeriod(p, day) for p in part]
+
+    def __getitem__(self, item):
+        return self.raw[item]
 
     def __repr__(self):
         return repr(self.raw)
