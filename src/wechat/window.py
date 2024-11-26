77ba4afb50128ebc99eaf9d4858e812091520e38
azazo1 <azazo1@qq.com>
2024-11-26 14:30:54 +0800

    wechat locate chat
    # Added
    
    - 微信窗口控制: 定位聊天框输入框.

--- src/wechat/window.py
+++ src/wechat/window.py
@@ -1,8 +1,8 @@
 """
 微信窗口控制.
 """
+from __future__ import annotations
 import uiautomation
-
 from .pc import get_wechat_window_control, WeChatError, wechat_control
 from ..config import logger, requires_init
 
@@ -26,10 +26,16 @@ class WeChat:
             logger.debug(f"close_window: {e}")
 
     @classmethod
-    def search(cls, text: str):
+    @requires_init
+    def search(cls, pattern: str) -> None:
         """
-        使用微信的搜索框搜索内容, 注意如果搜索内容不是联系人的名称的话,
-        可能会弹出搜索记录窗口, 对此脚本影响未知.
+        使用微信的搜索框搜索内容,
+            - 如果搜索内容是聊天窗口名称, 则会将微信定位至此聊天窗口.
+                - 当搜索到的聊天窗口以独立窗口存在的时候也能正常聚焦到聊天窗口.
+            - 如果搜索内容不是聊天窗口名称, 可能会弹出搜索记录窗口, 对此脚本影响未知.
+
+        Parameters:
+            pattern: 搜索字符串.
 
         Raises:
             WeChatError: 见 wechat_control 函数.
@@ -37,10 +43,61 @@ class WeChat:
         wechat_control().SetFocus()
         search_edit = wechat_control().EditControl(Depth=7, Name="搜索")
         search_edit.Click(simulateMove=False)
-        if uiautomation.SetClipboardText(text):
+        if uiautomation.SetClipboardText(pattern):
             search_edit.SendKeys("{Ctrl}a{Ctrl}v{Enter}")
         else:
             logger.error(f"search: failed to set clipboard.")
+
+    @classmethod
+    def locate_chat(cls, name: str | None = None) -> uiautomation.EditControl:
+        """
+        获取当前聊天窗口输入框的 Control.
+        支持获取独立聊天窗口中的输入框和微信主窗口中的聊天框.
+
+        此方法不会执行聊天框搜索操作, 需要提前手动调用 search 方法将焦点至于指定的聊天框.
+
+        Parameters:
+            name: 聊天框的名称.
+
+        对于 name:
+
+        - 如果没有指定, 那么使用微信主窗口的聊天输入框.
+            - 如果主窗口没有聊天框, 报 LookupError.
+        - 如果指定了,
+            - 如果当前微信主窗口处于 name 对应的聊天框, 使用此聊天框.
+            - 如果当前微信主窗口不处于 name 对应的聊天框, 在独立窗口中寻找 name 聊天窗口,
+                - 如果找到了此聊天窗口, 使用此聊天框.
+                - 如果没找到, 报 LookupError.
+
+
+        Raises:
+            WeChatError: 见 wechat_control 函数.
+            LookupError: 找不到微信中的指定组件, 可能是没进入聊天框, 可能是 name 指定的聊天框不存在.
+        """
+        ec = wechat_control().EditControl(Depth=13)  # 主窗口聊天框.
+        if ec.Exists(0, 0) and ec.Name == (name or ec.Name):
+            return ec
+        if name is None:
+            # 没有指定 name 时, 无法对独立窗口进行搜索, 故直接报错.
+            raise LookupError("failed to locate chat in main wechat window.")
+        standalone = uiautomation.WindowControl(searchDepth=1, ClassName="ChatWnd", Name=name)
+        ec = standalone.EditControl(Name="输入")
+        if ec.Exists(0, 0) and ec.Name == "输入":
+            return ec
+        raise LookupError(f"failed to locate chat: {name}")
+
+    @classmethod
+    @requires_init
+    def send_message(cls, text: str):
+        """
+        向当前聊天窗口发送文字信息, 请注意先调用 search 来定位聊天窗口.
+
+        Parameters:
+            text: 发送的消息.
+
+        Raises:
+            WeChatError: 见 wechat_control 函数.
+        """
 
 
 wx = WeChat  # 使用时直接使用 wx.xxx() 即可.
--- test/test_wechat/test_window.py
+++ test/test_wechat/test_window.py
@@ -1,4 +1,3 @@
-import os
 import timeit
 import unittest
 
@@ -9,7 +8,7 @@ from src.wechat.pc import get_pid_by_name
 
 class TestWechat(unittest.TestCase):
     def setUp(self):
-        init() # 自动移动到项目目录.
+        init()  # 自动移动到项目目录.
 
     def test_close_window(self):
         wx.close_window()
@@ -19,5 +18,32 @@ class TestWechat(unittest.TestCase):
 
     def test_bench_get_pid_by_name(self):
         timeit.main(['-s', 'from src.wechat.pc import get_pid_by_name',
+                     '-r', '10',
                      "get_pid_by_name('wechat.exe')"])
+        repeat_times = 10
+        bench_time = timeit.timeit(lambda: get_pid_by_name("wechat.exe"), number=repeat_times)
+        logger.info("%f / %d = %f sec/times", bench_time, repeat_times, bench_time / repeat_times)
         logger.info(get_pid_by_name("wechat.exe"))
+
+    def test_locate_chat(self):
+        """
+        请在指定的微信聊天框输入 asdf, 然后运行测试.
+
+        请测试各种情况:
+
+        1. 指定聊天窗口在微信主窗口内.
+        2. 指定聊天窗口在独立窗口.
+        3. 单个独立聊天窗口.
+        4. 多个独立聊天窗口.
+        5. 聊天窗口在主窗口内但是没被聚焦, 即当前聊天窗口不是指定聊天窗口.
+
+        以上情况可结合测试.
+        """
+        ec = wx.locate_chat("WechatTest")
+        vp = ec.GetValuePattern()
+        self.assertEqual(vp.Value, "asdf")
+
+    def test_bench_locate_chat(self):
+        timeit.main(['-s', 'from src.wechat import wx',
+                     "-r", "10",
+                     "wx.locate_chat()"])
